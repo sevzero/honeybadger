d92bab5ae8c19d2d8f4b2847afde575ba9acb23f
Russell <russell.hole@gmail.com>
2015-02-20 14:27:12 +0000

    Fixed single javascript samples reporting themselves in a script change

--- modules/payload_builder.py
+++ modules/payload_builder.py
@@ -5,25 +5,24 @@ import re
 
 class Payload:
 	def __init__(self, submission_id, code, analysis_time, code_format, honeybadger_js):
-		html_bootstrap_js = '''<meta http-equiv="refresh" content="%s;url=%s" />
-		<script type="text/javascript">
+		html_bootstrap_js = '''
 			honeybadger_submission_id = "%d"
 			honeybadger_reporting_url = "%s"
 			%s
-		</script>''' % (analysis_time, URL('done', args=[submission_id]), submission_id, URL('report'), honeybadger_js)
+		''' % (submission_id,URL('report'), honeybadger_js)
 		if code_format == 'html' and '<head' in code:
-			self.code = re.sub('< *head[^>]*>', '<head>' + html_bootstrap_js, code)
+			self.code = re.sub('< *head[^>]*>', '<head><script type="text/javascript">' + html_bootstrap_js + '</script>', code)
 			return
 		if (not '<script' in code and not '<html' in code) or code_format == 'js':
-			self.code = '<html><head>' + html_bootstrap_js + '</head><body><script>'+code+'</script></body></html>'
+			self.code = '<html><head></head></head><body><script type="text/javascript">'+html_bootstrap_js+code+'</script></body></html>'
 			return
 		if code.startswith('<script'):
-			self.code = '<html><head>' + html_bootstrap_js + '</head><body>'+code+'</body></html>'
+			self.code = '<html><head><script type="text/javascript">' + html_bootstrap_js + '</script></head><body>'+code+'</body></html>'
 			return
 		if '<head' in code:
-			self.code = re.sub('< *head[^>]*>', '<head>' + html_bootstrap_js, code)
+			self.code = re.sub('< *head[^>]*>', '<head><script type="text/javascript">' + html_bootstrap_js + '</script>', code)
 			return
 		if '<html' in code:
-			self.code = re.sub('< *html[^>]*>', '<html><head>' + html_bootstrap_js + '</head>', code)
+			self.code = re.sub('< *html[^>]*>', '<html><head><script type="text/javascript">' + html_bootstrap_js + '</script></head>', code)
 			return
 		self.code = code
