74f82c01769de37f765b0fd461858d06f6e786cf
Russell <russell.hole@gmail.com>
2015-02-23 16:01:27 +0000

    Replaced honeybadger base64 encoder with shorter one

--- static/js/honeybadger.js
+++ static/js/honeybadger.js
@@ -1,137 +1,48 @@
 // Obfuscation? Honey badger don't care!
 
-// Base64 function from http://stackoverflow.com/questions/246801/how-can-you-encode-a-string-to-base64-in-javascript#246813
-var Base64 = {
-	// private property
-	_keyStr : "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
-
-	// public method for encoding
-	encode : function (input) {
-		var output = "";
-		var chr1, chr2, chr3, enc1, enc2, enc3, enc4;
-		var i = 0;
+function honeybadger_base64_encode(data) {
+	//  discuss at: http://phpjs.org/functions/honeybadger_base64_encode/
+	// original by: Tyler Akins (http://rumkin.com)
+	// improved by: Bayron Guevara
+	// improved by: Thunder.m
+	// improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+	// improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+	// improved by: Rafał Kukawski (http://kukawski.pl)
+	// bugfixed by: Pellentesque Malesuada
 	
-		input = Base64._utf8_encode(input);
+	var b64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+	var o1, o2, o3, h1, h2, h3, h4, bits, i = 0,
+	ac = 0,
+	enc = '',
+	tmp_arr = [];
 	
-		while (i < input.length) {
-	
-			chr1 = input.charCodeAt(i++);
-			chr2 = input.charCodeAt(i++);
-			chr3 = input.charCodeAt(i++);
-	
-			enc1 = chr1 >> 2;
-			enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
-			enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
-			enc4 = chr3 & 63;
-	
-			if (isNaN(chr2)) {
-				enc3 = enc4 = 64;
-			} else if (isNaN(chr3)) {
-				enc4 = 64;
-			}
-	
-			output = output +
-			Base64._keyStr.charAt(enc1) + Base64._keyStr.charAt(enc2) +
-			Base64._keyStr.charAt(enc3) + Base64._keyStr.charAt(enc4);
-	
-		}
-	
-		return output;
-	},
-
-	// public method for decoding
-	decode : function (input) {
-		var output = "";
-		var chr1, chr2, chr3;
-		var enc1, enc2, enc3, enc4;
-		var i = 0;
-	
-		input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
-	
-		while (i < input.length) {
-	
-			enc1 = Base64._keyStr.indexOf(input.charAt(i++));
-			enc2 = Base64._keyStr.indexOf(input.charAt(i++));
-			enc3 = Base64._keyStr.indexOf(input.charAt(i++));
-			enc4 = Base64._keyStr.indexOf(input.charAt(i++));
-	
-			chr1 = (enc1 << 2) | (enc2 >> 4);
-			chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
-			chr3 = ((enc3 & 3) << 6) | enc4;
-	
-			output = output + String.fromCharCode(chr1);
-	
-			if (enc3 != 64) {
-				output = output + String.fromCharCode(chr2);
-			}
-			if (enc4 != 64) {
-				output = output + String.fromCharCode(chr3);
-			}
-	
-		}
-	
-		output = Base64._utf8_decode(output);
-	
-		return output;
-	
-	},
-
-	// private method for UTF-8 encoding
-	_utf8_encode : function (string) {
-		var utftext = "";
-	
-		for (var n = 0; n < string.length; n++) {
-	
-			var c = string.charCodeAt(n);
-	
-			if (c < 128) {
-				utftext += String.fromCharCode(c);
-			}
-			else if((c > 127) && (c < 2048)) {
-				utftext += String.fromCharCode((c >> 6) | 192);
-				utftext += String.fromCharCode((c & 63) | 128);
-			}
-			else {
-				utftext += String.fromCharCode((c >> 12) | 224);
-				utftext += String.fromCharCode(((c >> 6) & 63) | 128);
-				utftext += String.fromCharCode((c & 63) | 128);
-			}
-	
-		}
-	
-		return utftext;
-	},
-
-	// private method for UTF-8 decoding
-	_utf8_decode : function (utftext) {
-		var string = "";
-		var i = 0;
-		var c = c1 = c2 = 0;
-	
-		while ( i < utftext.length ) {
-	
-			c = utftext.charCodeAt(i);
-	
-			if (c < 128) {
-				string += String.fromCharCode(c);
-				i++;
-			}
-			else if((c > 191) && (c < 224)) {
-				c2 = utftext.charCodeAt(i+1);
-				string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
-				i += 2;
-			}
-			else {
-				c2 = utftext.charCodeAt(i+1);
-				c3 = utftext.charCodeAt(i+2);
-				string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
-				i += 3;
-			}
-	
-		}
-		return string;
+	if (!data) {
+		return data;
 	}
+	
+	do { // pack three octets into four hexets
+		o1 = data.charCodeAt(i++);
+		o2 = data.charCodeAt(i++);
+		o3 = data.charCodeAt(i++);
+		
+		bits = o1 << 16 | o2 << 8 | o3;
+		
+		h1 = bits >> 18 & 0x3f;
+		h2 = bits >> 12 & 0x3f;
+		h3 = bits >> 6 & 0x3f;
+		h4 = bits & 0x3f;
+		
+		// use hexets to index into b64, and append result to encoded string
+		tmp_arr[ac++] = b64.charAt(h1) + b64.charAt(h2) + b64.charAt(h3) + b64.charAt(h4);
+	} while (i < data.length);
+	
+	enc = tmp_arr.join('');
+	
+	var r = data.length % 3;
+	
+	return (r ? enc.slice(0, r - 3) : enc) + '==='.slice(r || 3);
 }
+
 
 // We'll use these later
 honeybadger_eval = eval
@@ -151,7 +62,7 @@ function honeybadger_log(type, msg){
 	else{// code for IE6, IE5
 		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
 	}
-	xmlhttp.open("POST", honeybadger_reporting_url+"?submission_id="+honeybadger_submission_id+"&type="+type+"&msg="+Base64.encode(msg).replace('+', '%2B'), false)
+	xmlhttp.open("POST", honeybadger_reporting_url+"?submission_id="+honeybadger_submission_id+"&type="+type+"&msg="+honeybadger_base64_encode(msg).replace('+', '%2B'), false)
 	xmlhttp.send(null);
 	return xmlhttp.responseText;
 }
@@ -231,7 +142,7 @@ window.setTimeout(function(){
 	result = compareThings(honeybadger_things, snapshot());
 	for (i in result){
 		if (result[i][1] == 'string'){
-			honeybadger_log('var', result[i][0] + ':' + Base64.encode(result[i][2]));
+			honeybadger_log('var', result[i][0] + ':' + honeybadger_base64_encode(result[i][2]));
 		}
 	}
 }, 8000);
