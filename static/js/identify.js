52d5020fa6f0795915b6679c44cc0368b06a678d
Russell <russell.hole@gmail.com>
2015-07-29 11:07:54 +0100

    Added script identification functionality

---
+++ static/js/identify.js
@@ -0,0 +1,25 @@
+function addIdentification(msg){
+	var identificationContainer = $('<div class="alert alert-info" role="alert"></div>');
+	var icon = $('<span class="glyphicon glyphicon-record" aria-hidden="true"></span>');
+	var identification = $('<span> Identification: ' + msg + '</span>');
+	identificationContainer.append(icon);
+	identificationContainer.append(identification);
+	$('#identifications').append(identificationContainer);
+}
+
+identificationChecks = [
+	{
+		msg: "AnglerEK",
+		check: function (){
+			return $('#analysis').html().search(/var cryptkey = /) != -1;
+		}
+	},
+];
+
+$( document ).ready(function() {
+	for (var i = 0; i < identificationChecks.length; i++){
+		if (identificationChecks[i].check()){
+			addIdentification(identificationChecks[i].msg);
+		}
+	}
+});
